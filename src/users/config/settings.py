965bff78bddff6a56b4f782b6fceaa61effc97e4
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-05-21 14:47:52 +0200

    base config

---
+++ src/users/config/settings.py
@@ -0,0 +1,43 @@
+from pydantic import BaseSettings, EmailStr, AnyHttpUrl
+from typing import List, Optional
+
+
+class Settings(BaseSettings):
+    # Project
+    PROJECT_NAME: str = "Online Cinema"
+    DEBUG: bool = True
+
+    # Security
+    SECRET_KEY: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    # Database
+    DATABASE_URL: str
+
+    # Email (for activation and reset password)
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASSWORD: str
+    EMAILS_FROM_EMAIL: EmailStr
+    EMAILS_FROM_NAME: Optional[str] = "Online Cinema Support"
+
+
+    # Token Expiration
+    ACTIVATION_TOKEN_EXPIRE_HOURS: int = 24
+    PASSWORD_RESET_TOKEN_EXPIRE_HOURS: int = 3
+
+    # Celery
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
