880a9970c2e98479494c79c5b895cc1196aabdbf
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-05-30 15:57:25 +0200

    added routes for movies, genres, and stars

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from users.router import router as users_router
+from src.movies.router.movies import router as movies_router
+from src.movies.router.genres import router as genres_router
+from src.movies.router.stars import router as stars_router
 
 app = FastAPI(
     title="Online Cinema",
@@ -10,4 +13,25 @@ app = FastAPI(
 
 api_version_prefix = "/api/v1"
 
-app.include_router(users_router, prefix=f"{api_version_prefix}/users", tags=["users"])
+app.include_router(
+    users_router,
+    prefix=f"{api_version_prefix}/users",
+    tags=["users"]
+)
+app.include_router(
+    movies_router,
+    prefix=f"{api_version_prefix}/movies",
+    tags=["movies"]
+)
+
+app.include_router(
+    genres_router,
+    prefix=f"{api_version_prefix}/genres",
+    tags=["genres"]
+)
+
+app.include_router(
+    stars_router,
+    prefix=f"{api_version_prefix}/stars",
+    tags=["stars"]
+)
