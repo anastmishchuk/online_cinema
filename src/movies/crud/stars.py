6c111f2c8a5e8fc9e7c9a2af1334f747aa72960a
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-05-30 15:55:04 +0200

    created crud operations & routers for stars

---
+++ src/movies/crud/stars.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from src.movies.models import Star
+from src.movies.schemas import StarCreate, StarUpdate
+
+
+async def get_star_by_id(db: AsyncSession, star_id: int) -> Star | None:
+    result = await db.execute(select(Star).where(Star.id == star_id))
+    return result.scalar_one_or_none()
+
+
+async def get_all_stars(db: AsyncSession) -> list[Star]:
+    result = await db.execute(select(Star))
+    return result.scalars().all()
+
+
+async def create_star(db: AsyncSession, star_in: StarCreate) -> Star:
+    star = Star(**star_in.dict())
+    db.add(star)
+    await db.commit()
+    await db.refresh(star)
+    return star
+
+
+async def update_star(db: AsyncSession, star_id: int, star_in: StarUpdate) -> Star:
+    star = await get_star_by_id(db, star_id)
+    if not star:
+        raise HTTPException(status_code=404, detail="Star is not found")
+
+    if star_in.name is not None:
+        star.name = star_in.name
+
+    await db.commit()
+    await db.refresh(star)
+    return star
+
+
+async def delete_star(db: AsyncSession, star_id: int) -> None:
+    star = await get_star_by_id(db, star_id)
+    if not star:
+        raise HTTPException(status_code=404, detail="Star is not found")
+
+    await db.delete(star)
+    await db.commit()
---
+++ src/movies/router/stars.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+
+from src.users.config.database import get_async_db
+from src.users.models import User
+from src.users.permissions import is_moderator
+from src.movies.schemas import StarCreate, StarUpdate, StarRead
+from src.movies.crud.stars import (
+    create_star,
+    get_star_by_id,
+    get_all_stars,
+    update_star,
+    delete_star,
+)
+
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[StarRead])
+async def list_stars(
+    db: AsyncSession = Depends(get_async_db),
+):
+    return await get_all_stars(db)
+
+
+@router.get("/{star_id}", response_model=StarRead)
+async def get_star(
+    star_id: int,
+    db: AsyncSession = Depends(get_async_db),
+):
+    star = await get_star_by_id(db, star_id)
+    if not star:
+        raise HTTPException(status_code=404, detail="Star is not found")
+    return star
+
+
+@router.post("/", response_model=StarRead, status_code=status.HTTP_201_CREATED)
+async def create_star_view(
+    star_in: StarCreate,
+    db: AsyncSession = Depends(get_async_db),
+    user: User = Depends(is_moderator)
+):
+    return await create_star(db, star_in)
+
+
+@router.put("/{star_id}", response_model=StarRead)
+async def update_star_view(
+    star_id: int,
+    star_in: StarUpdate,
+    db: AsyncSession = Depends(get_async_db),
+    user: User = Depends(is_moderator)
+):
+    return await update_star(db, star_id, star_in)
+
+
+@router.delete("/{star_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_star_view(
+    star_id: int,
+    db: AsyncSession = Depends(get_async_db),
+    user: User = Depends(is_moderator)
+):
+    await delete_star(db, star_id)
