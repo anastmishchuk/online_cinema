6565228ef022fe8a02bb509b5d5c785f504bb984
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-05-30 21:29:10 +0200

    add missed auth router to main app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from users.router import router as users_router
+from users.auth.router import router as auth_router
+
 from src.movies.router.movies import router as movies_router
 from src.movies.router.genres import router as genres_router
 from src.movies.router.stars import router as stars_router
@@ -18,6 +20,13 @@ app.include_router(
     prefix=f"{api_version_prefix}/users",
     tags=["users"]
 )
+
+app.include_router(
+    auth_router,
+    prefix=f"{api_version_prefix}/auth",
+    tags=["auth"]
+)
+
 app.include_router(
     movies_router,
     prefix=f"{api_version_prefix}/movies",
