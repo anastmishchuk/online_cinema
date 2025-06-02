8e247eefa42123879055b10cdcf3738622c7e7e9
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-06-02 17:55:22 +0200

    move Base declaration to database config

--- src/config/database.py
+++ src/config/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import (
     AsyncSession, async_session,
     create_async_engine, async_sessionmaker)
 
-from src.users.config.settings import settings
+from src.config.settings import settings
 
 
 sync_engine = create_engine(settings.SYNC_DATABASE_URL)
@@ -12,6 +12,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sync_engine)
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
 
 
 async def get_async_db() -> AsyncSession:
