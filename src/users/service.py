56693b808beae555e85cff47c5b3296fde30840f
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-07-06 17:29:03 +0200

    corrected reset link in send_password_reset_email

--- src/users/service.py
+++ src/users/service.py
@@ -11,20 +11,10 @@ async def send_activation_email(to_email: str, token: str):
     await send_email(to_email, subject, body)
 
 
-async def send_reset_password_email(to_email: str, token: str, new_password: str):
-    link = (f"{settings.BASE_URL}"
-            f"{settings.API_VERSION_PREFIX}"
-            f"{settings.USERS_ROUTE_PREFIX}/reset-password/"
-            f"{token}?new_password={new_password}")
-    subject = "Reset your password"
-    body = f"Click the link to reset your password: {link}"
-    await send_email(to_email, subject, body)
-
-
 async def send_password_reset_email(to_email: str, token: str):
     reset_link = (f"{settings.BASE_URL}"
                   f"{settings.API_VERSION_PREFIX}"
-                  f"{settings.USERS_ROUTE_PREFIX}/reset-password?token={token}")
+                  f"{settings.AUTH_ROUTE_PREFIX}/password/reset?token={token}")
     subject = "Reset your password"
     body = f"Click the link to reset your password: {reset_link}"
     await send_email(to_email, subject, body)
