1fb54b2ab6898df053dafa85fae9717af0348f06
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2025-07-06 17:28:20 +0200

    changed order of schemas

--- src/users/schema.py
+++ src/users/schema.py
@@ -1,24 +1,36 @@
 from datetime import datetime, date
 from typing import Optional
-from pydantic import BaseModel, EmailStr, ConfigDict, field_validator
+from pydantic import BaseModel, EmailStr, ConfigDict, field_validator, constr
 
 from src.users.models import UserGroupEnum
+from src.users.validators import validate_password_complexity
 
 
-class UserProfileRead(BaseModel):
-    first_name: Optional[str]
-    last_name: Optional[str]
-    avatar: Optional[str]
-    date_of_birth: Optional[date]
-    info: Optional[str]
+class UserRegisterSchema(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8, max_length=100)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "user@example.com",
+                "password": "StrongP@ssw0rd"
+            }
+        }
+
+    @field_validator("password")
+    def password_complexity(cls, v):
+        validate_password_complexity(v)
+        return v
 
 
-class UserProfileUpdate(BaseModel):
-    first_name: Optional[str]
-    last_name: Optional[str]
-    avatar: Optional[str]
-    date_of_birth: Optional[date]
-    info: Optional[str]
+class UserCreateSchema(BaseModel):
+    email: EmailStr
+    hashed_password: str
+    group: UserGroupEnum = UserGroupEnum.USER
+    is_active: Optional[bool] = False
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UserReadSchema(BaseModel):
@@ -31,13 +43,31 @@ class UserReadSchema(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-    @field_validator('group', mode='before')
+    @field_validator("group", mode="before")
     @classmethod
-    def serialize_group(cls, v):
-        """Convert UserGroup object to enum value for response."""
+    def extract_group_name(cls, v):
+        # Extract the enum value from the UserGroup object
         if hasattr(v, 'name'):
             return v.name
         return v
 
+
 class RoleChangeSchema(BaseModel):
     new_role: UserGroupEnum
+
+
+class UserProfileRead(BaseModel):
+    user_id: int
+    first_name: Optional[str]
+    last_name: Optional[str]
+    avatar: Optional[str]
+    date_of_birth: Optional[date]
+    info: Optional[str]
+
+
+class UserProfileUpdate(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    avatar: Optional[str] = None
+    date_of_birth: Optional[date] = None
+    info: Optional[str] = None
